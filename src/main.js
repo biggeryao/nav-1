fbcae3bac5a867f53c8be0dce4a2d54f2f85f228
bigyao <1040123159@qq.com>
2020-05-22 01:49:23 +0800

    前航项目

---
+++ src/main.js
@@ -0,0 +1,75 @@
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')
+const x = localStorage.getItem('x')
+const xObject = JSON.parse(x)
+const hashMap = xObject || [
+    { logo: 'A', url: 'https://www.acfun.cn' },
+    { logo: 'B', url: 'https://www.bilibili.com' }
+]
+const simplifyUrl = (url) => {
+    return url.replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .replace(/\/.*/, '')//删除  / 开头的内容
+}
+const render = () => {
+    $siteList.find('li:not(.last').remove()
+    hashMap.forEach((node, index) => {
+        const $li = $(`<li>
+            <div class="site">
+                <div class="logo">${node.logo[0]}</div>
+                <div class="link">${simplifyUrl(node.url)}</div>
+                <div class='close'>
+                    <svg class="icon" >
+                        <use xlink:href="#icon-close"></use>
+                    </svg>
+                </div>
+            </div>
+    </li>`).insertBefore($lastLi)
+        $li.on('click', () => {
+            console.log(1);
+
+            window.open(node.url)
+        })
+        $li.on('click', '.close', (e) => {
+            e.stopPropagation()//阻止冒泡
+            hashMap.splice(index, 1)
+            render()
+        })
+    })
+}
+
+render()
+
+$('.addButton').on('click', () => {
+    let url = window.prompt('请问你要添加的网址是啥')
+    if (url.indexOf('http') !== 0) {
+        url = 'https://' + url
+        console.log(url);
+    }
+    hashMap.push({
+        logo: simplifyUrl(url)[0].toUpperCase(),
+        url: url
+    })
+    render()
+})
+window.onbeforeunload = () => {
+    console.log('11');
+    const string = JSON.stringify(hashMap)
+    localStorage.setItem('x', string)
+}
+$(document).on('keypress', (e) => {
+    const key = e.key
+    for (let i = 0; i < hashMap.length; i++) {
+        if (hashMap[i].logo.toLowerCase() === key) {
+            window.open(hashMap[i].url)
+        }
+    }
+
+})
+//当点击了搜索框  ，阻止事件冒泡
+$(search).on('keypress', function (e) {
+    e = e || window.event;
+    if (e.stopPropagation) e.stopPropagation();
+    else e.cancelBubble = true;
+})
